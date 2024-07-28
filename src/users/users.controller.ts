92985cf175abb46ad869eca5ef9f115aec9475a7
John-Thailand <naoki.honda@mirrorfit.jp>
2024-07-28 12:30:25 +0900

    Provider

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,43 +1,58 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
 
 // コントローラーは、受信リクエストを処理し、クライアントに応答を返す責任があります。
 @Controller('users')
 export class UsersController {
-  /*
-  GET /users
-  GET /users/:id
-  POST /users
-  PATCH /users/:id
-  DELETE /users/:id
-  */
+  constructor(private readonly usersService: UsersService) {}
 
   @Get()
   findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    return [];
-  }
-
-  @Get('interns')
-  findAllInterns() {
-    return [];
+    return this.usersService.findAll(role);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return { id };
+    // Unary Plus: 文字列を数値に変換する
+    return this.usersService.findOne(+id);
   }
 
   @Post()
-  create(@Body() user: {}) {
-    return user;
+  create(
+    @Body()
+    user: {
+      name: string;
+      email: string;
+      role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.create(user);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() userUpdate: {}) {
-    return { id, ...userUpdate };
+  update(
+    @Param('id') id: string,
+    @Body()
+    userUpdate: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.update(+id, userUpdate);
   }
 
   @Delete(':id')
   delete(@Param('id') id: string) {
-    return { id };
+    return this.usersService.delete(+id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,92 @@
 import { Injectable } from '@nestjs/common';
 
+// クラスがDI（依存性注入）コンテナに登録されることを示します
+// 今回の例ですと、users.controller.tsでUsersServiceの依存関係として注入している
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'naoki',
+      email: 'test@gmail.com',
+      role: 'INTERN',
+    },
+    {
+      id: 2,
+      name: 'yoshiki',
+      email: 'test1@gmail.com',
+      role: 'INTERN',
+    },
+    {
+      id: 3,
+      name: 'rock',
+      email: 'test3@gmail.com',
+      role: 'ENGINEER',
+    },
+    {
+      id: 4,
+      name: 'mush',
+      email: 'test4@gmail.com',
+      role: 'ENGINEER',
+    },
+    {
+      id: 5,
+      name: 'sanji',
+      email: 'test5@gmail.com',
+      role: 'ADMIN',
+    },
+  ];
+
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+    return this.users;
+  }
+
+  findOne(id: number) {
+    const user = this.users.find((user) => user.id === id);
+
+    return user;
+  }
+
+  create(user: {
+    name: string;
+    email: string;
+    role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+  }) {
+    const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id);
+    const newUser = {
+      id: usersByHighestId[0].id + 1,
+      ...user,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(
+    id: number,
+    updatedUser: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return { ...user, ...updatedUser };
+      }
+      return user;
+    });
+
+    return this.findOne(id);
+  }
+
+  delete(id: number) {
+    const removedUser = this.findOne(id);
+
+    this.users = this.users.filter((usere) => usere.id !== id);
+
+    return removedUser;
+  }
+}
