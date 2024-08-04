7a01f2f6b146fb3d8f23e6803d477495c480aa47
John-Thailand <naoki.honda@mirrorfit.jp>
2024-08-04 15:33:33 +0900

    スロットルを設定し、APIの実行回数に制限を設定する

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,43 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { EmployeesModule } from './employees/employees.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, EmployeesModule],
+  imports: [
+    UsersModule,
+    DatabaseModule,
+    EmployeesModule,
+    // スロットリングを提供するモジュール
+    // スロットリング：システムの過負荷や特定利用者による資源の独占を回避するため
+    // 一定の制限値を超えた場合に意図的に性能を低下させたり、要求を一時的に拒否したりする制御のこと
+    ThrottlerModule.forRoot([
+      {
+        // レート制限に名前を付けている
+        name: 'short',
+        // time-to-live: 時間の長さ。この場合は60000秒
+        ttl: 1000,
+        // 指定されたttlの間に許可されるリクエストの最大数
+        limit: 3,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      // APP_GUARD：NestJSのグローバルスコープのガードを設定するために使用されるトークン
+      // アプリ全体に適用されるガードを提供できる
+      provide: APP_GUARD,
+      // ThrottlerGuardがアプリケーション全体のグローバルガードとして設定される
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
