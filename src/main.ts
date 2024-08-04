80e645025755987a96ac4747e61e3c217455899f
John-Thailand <naoki.honda@mirrorfit.jp>
2024-08-04 16:02:32 +0900

    カスタムロガーを設定

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MyLoggerService } from './my-logger/my-logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    // アプリケーションが起動中にログをバッファリングするオプション
+    bufferLogs: true,
+  });
+  // カスタムロガーを設定
+  app.useLogger(app.get(MyLoggerService));
   app.enableCors();
   app.setGlobalPrefix('api');
   await app.listen(3000);
